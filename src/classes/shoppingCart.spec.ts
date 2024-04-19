460fc7fff775c883e6e3f36f5f8cafa4a2252e8f
Altamiro Júnior <124850908+AltamiroF-22@users.noreply.github.com>
2024-04-19 04:42:06 -0300

    Shoping cart test

---
+++ src/classes/shoppingCart.spec.ts
@@ -0,0 +1,68 @@
+import { Discount } from "./discount";
+import { Product } from "./interface/cartProduct";
+import { ShoppingCart } from "./shoppingCart";
+
+const creatSut = () => {
+  const discountMock = creaDiscountMock();
+  const sut = new ShoppingCart(discountMock);
+
+  return { discountMock, sut };
+};
+
+const creaDiscountMock = () => {
+  class DiscountMock extends Discount {}
+  return new DiscountMock();
+};
+
+const createCartProduct = (name: string, price: number) => {
+  class CartProductMock implements Product {
+    constructor(public name: string, public price: number) {}
+  }
+
+  return new CartProductMock(name, price);
+};
+
+const creatSutWithProduct = () => {
+  const { sut, discountMock } = creatSut();
+  const cartProduct1 = createCartProduct("T-Shirt", 134.55);
+  const cartProduct2 = createCartProduct("Pen", 1.55);
+  sut.addProduct(cartProduct1);
+  sut.addProduct(cartProduct2);
+
+  return { sut, discountMock };
+};
+
+describe("ShoppingCart", () => {
+  it("Should be an empty cart when no products was added", () => {
+    const { sut } = creatSut();
+    expect(sut.isEmpty()).toBe(true);
+  });
+
+  it("Should have 2 cart items", () => {
+    const { sut } = creatSutWithProduct();
+    expect(sut.product.length).toBe(2);
+  });
+
+  it("Should add products and clear cart", () => {
+    const { sut } = creatSutWithProduct();
+
+    expect(sut.product.length).toBe(2);
+    sut.clear();
+    expect(sut.product.length).toBe(0);
+    expect(sut.isEmpty()).toBe(true);
+  });
+
+  it("Should remove products", () => {
+    const { sut } = creatSutWithProduct();
+
+    expect(sut.product.length).toBe(2);
+
+    sut.removeProduct(sut.product.length - 1);
+    expect(sut.product.length).toBe(1);
+
+    sut.removeProduct(sut.product.length - 1);
+    expect(sut.product.length).toBe(0);
+
+    expect(sut.isEmpty()).toBe(true);
+  });
+});
